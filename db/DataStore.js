58f2795e7e46bd1f03a87df62075cf30ec82ff29
jerry <jerrynwosu007@gmail.com>
2021-08-18 16:57:45 +0100

    setup app store

---
+++ db/DataStore.js
@@ -0,0 +1,36 @@
+const Store = require("electron-store");
+
+class DataStore extends Store {
+  constructor(settings) {
+    super(settings);
+
+    // initialize with tasks or empty array
+    this.tasks = this.get("tasks") || [];
+  }
+
+  saveTasks() {
+    // save tasks to JSON file
+    this.set("tasks", this.tasks);
+    return this;
+  }
+
+  getTasks() {
+    // set object's tasks to tasks in JSON file
+    this.taks = this.get("tasks") || [];
+    return this;
+  }
+
+  addTask(task) {
+    // merge the existing tasks with the new task
+    this.tasks = [...this.tasks, task];
+    return this.saveTasks();
+  }
+
+  deleteTask(task) {
+    // filter out the target task
+    this.tasks = this.tasks.filter((t) => t !== task);
+    return this.saveTasks();
+  }
+}
+
+module.exports = DataStore;
---
+++ main.js
@@ -0,0 +1,4 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+
+const DataStore = require("./db/DataStore");
